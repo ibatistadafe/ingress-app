608cc7b6f91317f5a7b3d876648160722fcccdcf
Marcos Sales <m4rcos.sales@gmail.com>
2024-03-12 12:16:06 -0300

    subindo arquivos

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CreateFormComponent } from '../../components/create-form/create-form.component';
+import { HeaderComponent } from "../../components/header/header.component"
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [ CreateFormComponent],
+  imports: [ CreateFormComponent, HeaderComponent],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
