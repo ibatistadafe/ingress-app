33b75ef15d96ea56c74517c285b27f155845ac0b
Marcos Sales <m4rcos.sales@gmail.com>
2024-03-26 11:18:39 -0300

    alterações

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CreateFormComponent } from '../../components/create-form/create-form.component';
 import { HeaderComponent } from "../../components/header/header.component"
 import { LoaderFormComponent } from '../../components/loader-form/loader-form.component';
+import { ZipFormComponent } from '../../components/zip-form/zip-form.component';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [ CreateFormComponent, HeaderComponent, LoaderFormComponent ],
+  imports: [ CreateFormComponent, HeaderComponent, LoaderFormComponent, ZipFormComponent],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
