feb581b22a75ac291760fd55c87b6dca1108ac2f
Marcos Sales <m4rcos.sales@gmail.com>
2024-03-26 12:01:03 -0300

    criando form de senha

---
+++ src/app/components/password-form/password-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-password-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './password-form.component.html',
+  styleUrl: './password-form.component.scss'
+})
+export class PasswordFormComponent {
+
+}
--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -3,11 +3,19 @@ import { CreateFormComponent } from '../../components/create-form/create-form.co
 import { HeaderComponent } from "../../components/header/header.component"
 import { LoaderFormComponent } from '../../components/loader-form/loader-form.component';
 import { ZipFormComponent } from '../../components/zip-form/zip-form.component';
+import { PasswordFormComponent } from '../../components/password-form/password-form.component';
+
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [ CreateFormComponent, HeaderComponent, LoaderFormComponent, ZipFormComponent],
+  imports: [
+    CreateFormComponent,
+    HeaderComponent,
+    LoaderFormComponent,
+    ZipFormComponent,
+    PasswordFormComponent
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
