9d2d6345f9f5c6e8521b81cd34c2b5da0f6cb547
Marcos Sales <m4rcos.sales@gmail.com>
2024-04-01 16:42:00 -0300

    feat: add rotas para formulário

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,13 @@
 import { Routes } from '@angular/router';
 import { CreateFormComponent } from './components/create-form/create-form.component';
+import { ZipFormComponent } from './components/zip-form/zip-form.component';
+import { PasswordFormComponent } from './components/password-form/password-form.component';
+import { SuccessRouteComponent } from './pages/success-route/success-route.component';
 
 
 export const routes: Routes = [
-  { path: './pages/register/create-form', component: CreateFormComponent}
+  { path: '', component: CreateFormComponent},
+  { path: 'zipcode', component: ZipFormComponent},
+  { path: 'password', component: PasswordFormComponent},
+  { path: 'success', component: SuccessRouteComponent},
 ];
--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
-import { CreateFormComponent } from '../../components/create-form/create-form.component';
+
 import { HeaderComponent } from "../../components/header/header.component"
 import { LoaderFormComponent } from '../../components/loader-form/loader-form.component';
-import { ZipFormComponent } from '../../components/zip-form/zip-form.component';
-import { PasswordFormComponent } from '../../components/password-form/password-form.component';
-import { SuccessRouteComponent } from '../success-route/success-route.component';
 
+import { RouterOutlet } from '@angular/router';
+
+import { CreateFormComponent } from '../../components/create-form/create-form.component';
+import { PasswordFormComponent } from '../../components/password-form/password-form.component';
+import { ZipFormComponent } from '../../components/zip-form/zip-form.component';
+import { SuccessRouteComponent } from '../success-route/success-route.component';
 
 @Component({
   selector: 'app-register',
@@ -16,7 +19,8 @@ import { SuccessRouteComponent } from '../success-route/success-route.component'
     LoaderFormComponent,
     ZipFormComponent,
     PasswordFormComponent,
-    SuccessRouteComponent
+    SuccessRouteComponent,
+    RouterOutlet
   ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
