9d7e03405f4d7c32ddfc5c8b6b6d3d1d98b9e2ae
Marcos Sales <m4rcos.sales@gmail.com>
2024-04-24 17:06:56 -0300

    feat:validação do campo senha  e navegação, falta a confirmação

--- src/app/components/password-form/password-form.component.ts
+++ src/app/components/password-form/password-form.component.ts
@@ -1,12 +1,41 @@
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {  FormsModule, NgForm, AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
+import { NgxMaskDirective } from 'ngx-mask';
 
 @Component({
   selector: 'app-password-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule,NgxMaskDirective],
   templateUrl: './password-form.component.html',
   styleUrl: './password-form.component.scss'
 })
 export class PasswordFormComponent {
+  form = {
+    password: '',
+    confirmPassword:  '',
+    member: null
+  };
 
+  constructor(private router: Router){ }
+
+  // Função de validação para comparar as senhas
+  checkPasswords(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const password = control.get('password')?.value;
+      const confirmPassword = control.get('confirmPassword')?.value;
+
+      return password === confirmPassword ? null : { notSame: true };
+    };
+  }
+
+  onSubmit(): void {
+      console.log(JSON.stringify(this.form, null, 2));
+      this.router.navigate(["/success"]);
+  }
+
+  onReset(form: NgForm): void {
+    form.resetForm();
+  }
 }
