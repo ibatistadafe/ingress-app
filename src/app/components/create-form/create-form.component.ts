2bf5f800d63cd21e70c5528e6031385d2f31e60f
tomsales92 <everton.goncalves92@gmail.com>
2024-05-30 19:07:57 -0300

    Criação da validação do campos do formulário

--- src/app/components/create-form/create-form.component.ts
+++ src/app/components/create-form/create-form.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './create-form.component.html',
   styleUrl: './create-form.component.scss'
 })
-export class CreateFormComponent {
+export class CreateFormComponent implements OnInit {
 
+  form: FormGroup;
+  constructor(private fb: FormBuilder) { }
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      nome_completo: new FormControl('', Validators.required),
+      cpf: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      confirmar_email: new FormControl('', [Validators.required, this.verificaSeEmailsCoincidem('email')]),
+    })
+  }
+
+  verificaSeEmailsCoincidem(matchTo: string): (control: AbstractControl) => { [key: string]: boolean } | null {
+    return (control: AbstractControl) => {
+      return control.value === control.parent?.get(matchTo)?.value
+        ? null
+        : { emailsNaoCoincidem: true };
+    };
+  }
+
+ public enviaFormulario(): void {
+ console.log('Passamos aqui')
+  }
 }
