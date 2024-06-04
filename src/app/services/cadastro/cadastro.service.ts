066db51c1cdede735f7bae2bd13df39c104acdb0
tomsales92 <everton.goncalves92@gmail.com>
2024-06-04 11:57:39 -0300

    Criação de serviço de Salvamento do cadastro completo

---
+++ src/app/services/cadastro/cadastro.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { CadastroCompletoDTO } from '../../model/cadastro/cadastro-completo.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CadastroService {
+
+  constructor() { }
+
+  criaCadastro(value: CadastroCompletoDTO) {
+    
+  }
+}
